9bed22c9a56d6e4b2abaee560dd7a8d96e4b9433
jmicharkCode <88967855+jmicharkCode@users.noreply.github.com>
2022-03-16 15:09:19 +0700

    add welcome

---
+++ src/main/java/cybersoft/javabackend/java16girahiep/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package cybersoft.javabackend.java16girahiep.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	@GetMapping
+	public Object Welcome() {
+		return "Welcome to Gira Application";
+	}
+}
