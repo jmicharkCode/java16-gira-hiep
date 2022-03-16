5da7744293d4b6756f861a1461c4870c462b3071
jmicharkCode <88967855+jmicharkCode@users.noreply.github.com>
2022-03-16 15:13:16 +0700

    add Welcome API

--- src/main/java/cybersoft/javabackend/java16girahiep/controller/WelcomeController.java
+++ src/main/java/cybersoft/javabackend/java16girahiep/controller/WelcomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-	@GetMapping("welcome")
+	@GetMapping
 	public Object Welcome() {
 		return "Welcome to Gira Application";
 	}
