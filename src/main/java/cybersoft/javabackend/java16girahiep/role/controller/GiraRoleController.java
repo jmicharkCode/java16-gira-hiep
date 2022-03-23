f344300e134981e3caf94dc115237643994a9ce0
jmicharkCode <88967855+jmicharkCode@users.noreply.github.com>
2022-03-24 00:22:02 +0700

    j

--- src/main/java/cybersoft/javabackend/java16girahiep/role/controller/GiraRoleController.java
+++ src/main/java/cybersoft/javabackend/java16girahiep/role/controller/GiraRoleController.java
@@ -1,10 +1,15 @@
 package cybersoft.javabackend.java16girahiep.role.controller;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,7 +33,14 @@ public class GiraRoleController {
 	}
 	
 	@PostMapping
-	public Object createNewRole(@RequestBody GiraRoleDTO dto) {
+	public Object createNewRole(@Valid @RequestBody GiraRoleDTO dto,
+								BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			return new ResponseEntity<>(bindingResult.getAllErrors()
+					.stream().map(t -> t.getDefaultMessage()).collect(Collectors.toList())
+					, HttpStatus.BAD_REQUEST);
+		}
+		
 		GiraRole role = service.save(dto);
 		return new ResponseEntity<>(role, HttpStatus.CREATED);
 	}
--- src/main/java/cybersoft/javabackend/java16girahiep/role/service/GiraRoleService.java
+++ src/main/java/cybersoft/javabackend/java16girahiep/role/service/GiraRoleService.java
@@ -2,6 +2,7 @@ package cybersoft.javabackend.java16girahiep.role.service;
 
 import java.util.List;
 
+
 import cybersoft.javabackend.java16girahiep.role.dto.GiraRoleDTO;
 import cybersoft.javabackend.java16girahiep.role.model.GiraRole;
 
