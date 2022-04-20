1c8c93419a71c25216de64942d29ef77b012e269
jmicharkCode <88967855+jmicharkCode@users.noreply.github.com>
2022-04-20 19:16:49 +0700

    add security JWT

--- src/main/java/cybersoft/javabackend/java16girahiep/config/SwaggerConfig.java
+++ src/main/java/cybersoft/javabackend/java16girahiep/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package cybersoft.javabackend.java16girahiep.config;
 
+import java.util.List;
+
 import org.springframework.boot.autoconfigure.info.ProjectInfoProperties.Build;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,8 +9,12 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiDescriptionBuilder;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -18,6 +24,8 @@ public class SwaggerConfig {
 	@Bean
 	public Docket getDocket() {
 		return new Docket(DocumentationType.SWAGGER_2)
+				.securityContexts(List.of(getSecurityContext()))
+				.securitySchemes(List.of(getSecurityScheme()))
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("cybersoft.javabackend.java16girahiep"))
 				.build()
@@ -26,6 +34,24 @@ public class SwaggerConfig {
 						.description("This project í used for education purpose only.")
 						.contact(new Contact("hiep", "https://tuanphan.dev/", "jmichark@gmail.com"))
 						.build());
+	}
+	
+	private ApiKey getSecurityScheme() {
+		return new ApiKey("JWT", "Authorization", "header");
+	}
+	
+	private SecurityContext getSecurityContext() {
+		return SecurityContext.builder()
+				.securityReferences(List.of(getSecurityReference()))
+				.build();
+	}
+	
+	private SecurityReference getSecurityReference() {
+		AuthorizationScope globalScope = new AuthorizationScope("global", "use for all request");
+		AuthorizationScope scopes[] = new AuthorizationScope[1];
+		scopes[0] = globalScope;
+		
+		return new SecurityReference("JWT", scopes);
 	}
 
 }
--- src/main/java/cybersoft/javabackend/java16girahiep/security/config/SecurityConfig.java
+++ src/main/java/cybersoft/javabackend/java16girahiep/security/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +13,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import cybersoft.javabackend.java16girahiep.security.jwt.JwtAuthorizationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +23,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private UserDetailsService userDetailService;
 	
+	@Autowired
+	private JwtAuthorizationFilter jwtAuthFilter;
 	
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
@@ -49,9 +55,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http.csrf().disable();
 		
 		// JWL FILTER
+		http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+		
 		
 		// API AUTHENTICATION
 		http.antMatcher("/api/v1/**").authorizeRequests()
+			.antMatchers("/api/v1/auth/login").permitAll()
+			.antMatchers("/api/v1/users").permitAll()
 			.anyRequest().authenticated();
 	}
 	
